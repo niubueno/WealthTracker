632a076ed5ea77dbeb1e56bc91e7ab55d4a51436
Paulo Bueno <pbueno.niu@gmail.com>
2023-05-10 16:38:17 -0300

    fix: adjustments to get the price with yahho api

--- WealthTracker/Api/YahooFinanceApi.cs
+++ WealthTracker/Api/YahooFinanceApi.cs
@@ -20,7 +20,7 @@ namespace WealthTracker.Api
 
         private async Task<YahooFinanceQuote> GetStockQuote(string symbol)
         {
-            var url = $"https://query1.finance.yahoo.com/v7/finance/quote?symbols={symbol}";
+            var url = $"https://query1.finance.yahoo.com/v6/finance/quote?symbols={symbol}";
             var response = await _httpClient.GetAsync(url);
             var content = await response.Content.ReadAsStringAsync(); 
             var result = JsonConvert.DeserializeObject<YahooFinanceResult>(content);
--- WealthTracker/Controllers/StockDataController.cs
+++ WealthTracker/Controllers/StockDataController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
 using System.Text.Json;
 using WealthTracker.Api;
 using WealthTracker.Models;
@@ -8,16 +9,28 @@ namespace WealthTracker.Controllers
     public class StockDataController : Controller
     {
         private readonly HttpClient _httpClient;
+        private YahooFinanceApi _yahooFinanceApi;
 
         public StockDataController()
         {
             _httpClient = new HttpClient();
+            _yahooFinanceApi = new YahooFinanceApi(_httpClient);
         }
 
         [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetStockData([FromForm] Stock stock)
+        {
+            var price = await _yahooFinanceApi.GetStockPrice(stock.Symbol);
+            stock.CurrentPrice = price.ToString();
+            ViewBag.Quote = price;
+
+            return View("Index", stock);
         }
 
     }
